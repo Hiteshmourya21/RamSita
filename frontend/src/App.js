caee0e44ae4d309ff7465ad6e3e32313160a003f
Hitesh Mourya <143187032+Hiteshmourya21@users.noreply.github.com>
2025-01-27 10:40:48 +0530

    Remove ristriction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,55 @@
-// import React, { useState } from 'react';
+import React from 'react';
 import styles from './App.module.css';
 import Admin from './components/AdminDashBoard/Admin';
 import VenueDetail from './components/AdminDashBoard/VenueDetail';
 import AddAuthor from './components/Author/AddAuthor';
 import LoginForm from './components/frontpage/LoginForm';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate, Navigate } from 'react-router-dom';
 import SessionDashboard from './components/SessionChair/SessionDashboard';
 import AuthorDashboard from './components/Author/AuthorDashboard';
 import AddFaculty from './components/AddFaculty';
 import AddPresentationPage from './components/Author/AddPresentaionPage';
 
 const App = () => {
+  const token = localStorage.getItem('token');
+
   return (
-    <div className={styles.app}>
-      <Router>
+    <Router>
+      <div className={styles.app}>
         <Routes>
-          <Route path="/" element={< LoginForm />} />
-          <Route path="/admin/TrackDetail" element={<VenueDetail />} />
-          <Route path="/admin/addAuthor" element={<AddAuthor />} />
-          <Route path="/admin/dashboard" element={< Admin />} />
-          <Route path="/admin/addFaculty" element={< AddFaculty />} />
-          <Route path="/session/dashboard" element={< SessionDashboard />} />
-          <Route path="/author/dashboard" element={< AuthorDashboard />} />
-          <Route path="/add-presentation" element={< AddPresentationPage />} />
+          <Route path="/" element={<LoginForm />} />
+          <Route
+            path="/admin/TrackDetail"
+            element={!token ? <Navigate to="/" /> : <VenueDetail />}
+          />
+          <Route
+            path="/admin/addAuthor"
+            element={!token ? <Navigate to="/" /> : <AddAuthor />}
+          />
+          <Route
+            path="/admin/dashboard"
+            element={!token ? <Navigate to="/" /> : <Admin />}
+          />
+          <Route
+            path="/admin/addFaculty"
+            element={!token ? <Navigate to="/" /> : <AddFaculty />}
+          />
+          <Route
+            path="/session/dashboard"
+            element={!token ? <Navigate to="/" /> : <SessionDashboard />}
+          />
+          <Route
+            path="/author/dashboard"
+            element={!token ? <Navigate to="/" /> : <AuthorDashboard />}
+          />
+          <Route
+            path="/add-presentation"
+            element={!token ? <Navigate to="/" /> : <AddPresentationPage />}
+          />
         </Routes>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 };
 
 export default App;
-
