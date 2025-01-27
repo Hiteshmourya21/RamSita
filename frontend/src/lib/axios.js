f9f1b6ff2b8683cb8fdf09d5062a3266239e2ef5
Hitesh Mourya <143187032+Hiteshmourya21@users.noreply.github.com>
2025-01-27 12:02:11 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: "/api/v1" ,
-    baseURL: "http://localhost:5000/api/v1",
+     baseURL: "/api/v1" ,
+    //baseURL: "http://localhost:5000/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
